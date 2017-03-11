87c7bd15b91c554b649fcabfe54f1fe7c35229f5
Seth Balodi <shashwb@vt.edu>
2017-03-11 12:51:01 -0500

    read in

---
+++ calculator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main (int argc, const char* argv[]) {
+  cout << "Welcome to Calculator" << endl;
+
+  //calculator needs to read in value from the command line
+
+  string input_expression = "";
+  string input_to_program = "";
+
+  string messageVar;
+  while (getline(input_expression)) {
+
+
+  }
+
+  cout << "input_to_program: " << input_to_program << endl;
+
+
+
+  return 1;
+}
